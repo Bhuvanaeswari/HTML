49ae1aa121574bd345f1a47abf12f36ab2d04372
bhuvanaeswari <bhu_ess@yahoo.co.in>
2022-02-22 12:48:48 +0530

    JAVASCRIPT FOR,FOREACH,FILTER,MAP,SORT,REDUCE

---
+++ JAVASCRIPT/SCRIPT/array.js
@@ -0,0 +1,9 @@
+const companies=[{name:"company one",category:"finance",start:1980,end:2009},
+{name="company two",category:"retail",start:1989,end:2000},
+{name="company three",category:"finance",start:2000,end:2022}
+];
+const ages=[23,45,23,11,3,67];
+for(let i=0;i<companies.length;i++)
+{
+    console.log(companies[i])
+}
